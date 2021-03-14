c523676a94baa2c2361cf9d33c759e2c10c3436b
Archit Khode <arkits@outlook.com>
2021-03-14 14:35:29 -0700

    dev: enhance version to include build info

--- cmd/version.go
+++ cmd/version.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/jedib0t/go-pretty/v6/text"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -14,8 +15,18 @@ var versionCmd = &cobra.Command{
 }
 
 func printCliVersion(cmd *cobra.Command, args []string) {
-	fmt.Printf("spring-boot-actuator-cli version %s \n", viper.GetString("application.version"))
+	// About
+	fmt.Println(text.Bold.Sprintf("sba-cli version: %s", viper.GetString("application.version")))
+	fmt.Println("A CLI utility to work with Spring Boot's Actuator Endpoint")
 	fmt.Println("")
-	fmt.Println("Here are some cool links: ")
+
+	// Build info
+	fmt.Println(text.Bold.Sprintf("Build Info:"))
+	fmt.Printf("- application.gitTag=%s \n", viper.GetString("application.gitTag"))
+	fmt.Printf("- application.gitCommit=%s \n", viper.GetString("application.gitCommit"))
+	fmt.Println("")
+
+	// Shilling
+	fmt.Println(text.Bold.Sprintf("Here are some cool links:"))
 	fmt.Println("- Project Github: https://github.com/arkits/spring-boot-actuator-cli")
 }
--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/arkits/spring-boot-actuator-cli/cmd"
 	"github.com/spf13/viper"
 )
 
 var (
-	version string
+	gitCommit string
+	gitTag    string
 )
 
 func main() {
 
-	if version == "" {
-		version = "unset"
-	}
-
-	viper.Set("application.version", version)
+	assembleVersion()
 
 	cmd.Execute()
+}
+
+func assembleVersion() {
+
+	if gitCommit == "" {
+		gitCommit = "__UNSET_COMMIT"
+	}
+
+	if gitTag == "" {
+		gitTag = "UNSET_TAG"
+	}
+
+	applicationVersion := fmt.Sprintf("%s:%s", gitTag, gitCommit[0:7])
+
+	viper.Set("application.version", applicationVersion)
+	viper.Set("application.gitCommit", gitCommit)
+	viper.Set("application.gitTag", gitTag)
+
 }
